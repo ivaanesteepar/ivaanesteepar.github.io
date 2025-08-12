3b7c4cff1e796d134fe45167374a6d505841cd93
Ivan Estepar Rebollo <ivan.estepar@csa.es>
2025-08-12 13:46:27 +0200

    Preparado para correción de loop infinito

--- js/script.js
+++ js/script.js
@@ -117,12 +117,12 @@ function initMobileMenu() {
 
 // Añade la animación al main cuando se entra a la página
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('main').classList.add('fadeInUp');
+    document.querySelector('main').classList.add('fadeInUp');
 });
 
 // Hace scroll up si se recarga la página
 window.addEventListener('load', () => {
-  window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
 });
 
 // Carga las animaciones cuando se recarga la página
@@ -136,39 +136,7 @@ document.addEventListener('DOMContentLoaded', () => {
     initMobileMenu();
 });
 
-const langContainer = document.querySelector('.container-languages ul');
-const techContainer = document.querySelector('.container-technologies ul');
 
-// 1. Duplicar contenido para efecto infinito sin huecos
-
-
-let langPos = 0;
-let techPos = 0;
-const speed = 0.5; // Ajusta la velocidad del desplazamiento
-
-function animate() {
-  // Ancho total del contenido original (sin duplicar)
-  const langWidth = langContainer.scrollWidth / 2;
-  const techWidth = techContainer.scrollWidth / 2;
-
-  // Mover languages a la izquierda con límite
-  langPos -= speed;
-  if (langPos <= -langWidth) {
-    langPos += langWidth; // Ajusta la posición para que el contenido duplicado se alinee
-  }
-  langContainer.style.transform = `translateX(${langPos}px)`;
-
-  // Mover technologies a la derecha con límite
-  techPos += speed;
-  if (techPos >= techWidth) {
-    techPos -= techWidth; // Ajusta la posición para que el contenido duplicado se alinee
-  }
-  techContainer.style.transform = `translateX(${techPos}px)`;
-
-  requestAnimationFrame(animate);
-}
-
-animate();
 
 
 
